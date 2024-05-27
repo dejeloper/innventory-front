7ba13265ca4493bb0d2d7850a7cc77e4b6da75b7
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-27 10:38:01 -0500

    Improve theme toggle button in ModeToggle component

--- src/components/theme-toggle-button.tsx
+++ src/components/theme-toggle-button.tsx
@@ -2,27 +2,20 @@
 
 import * as React from "react";
 import { useTheme } from "next-themes";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { NavbarButton } from "./app/navbar/navbar.button";
 import { Moon, Sun } from "lucide-react";
-import { Button } from "./ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
-
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
   return (
     <>
-      {theme === "light" ? (
-        <NavbarButton icon={Moon} onClick={() => setTheme("dark")} />
-      ) : (
-        <NavbarButton icon={Sun} onClick={() => setTheme("light")} />
-      )}
+      <NavbarButton
+        icon={theme === "light" ? Moon : Sun}
+        onClick={changeTheme}
+      />
     </>
   );
 }
