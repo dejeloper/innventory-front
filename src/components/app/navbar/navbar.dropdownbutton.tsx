c25fb228b526b74039846a6712e429a3797c9479
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-27 13:30:40 -0500

    Refactor NavbarDropdownButton component to support both icon and button types

--- src/components/app/navbar/navbar.dropdownbutton.tsx
+++ src/components/app/navbar/navbar.dropdownbutton.tsx
@@ -7,20 +7,37 @@ import {
 import { NavbarButton } from "./navbar.button";
 import { LucideIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+type ButtonVariant =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
 
 interface NavbarDropdownButtonProps {
-  icon: LucideIcon;
+  type?: "icon" | "button";
+  icon?: LucideIcon;
   iconSize?: number;
   iconClassname?: string;
+  buttonContent?: React.ReactNode;
+  buttonVariant?: ButtonVariant;
+  buttonClassname?: string;
   dropdownMenuContent: React.ReactNode;
   dropdownMenuContentClassName?: string;
   dropdownMenuLabel?: string;
 }
 
 export function NavbarDropdownButton({
+  type = "icon",
   icon,
   iconSize = 20,
   iconClassname,
+  buttonVariant,
+  buttonClassname,
+  buttonContent,
   dropdownMenuContent,
   dropdownMenuContentClassName,
   dropdownMenuLabel,
@@ -29,7 +46,18 @@ export function NavbarDropdownButton({
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div>
-          <NavbarButton icon={icon} size={iconSize} classname={iconClassname} />
+          {type === "icon" && icon && (
+            <NavbarButton
+              icon={icon}
+              size={iconSize}
+              classname={iconClassname}
+            />
+          )}
+          {type === "button" && buttonVariant && (
+            <Button variant={buttonVariant} className={buttonClassname}>
+              {buttonContent}
+            </Button>
+          )}
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className={cn(dropdownMenuContentClassName)}>
