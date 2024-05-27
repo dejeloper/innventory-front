0bfc1005b0c2f6c31bb6262bd660966f6497f5da
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-27 10:03:50 -0500

    Update ModeToggle component to use lucide-react icons and improve theme toggle button

--- src/components/theme-toggle-button.tsx
+++ src/components/theme-toggle-button.tsx
@@ -1,27 +1,30 @@
 "use client";
 
 import * as React from "react";
-import { RiMoonLine, RiSunLine } from "react-icons/ri";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { NavbarButton } from "./app/navbar/navbar.button";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <RiSunLine className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <RiMoonLine className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+        <Button variant={"ghost"} className="w-8 h-8 p-0">
+          {theme === "light" ? (
+            <NavbarButton icon={Sun} />
+          ) : (
+            <NavbarButton icon={Moon} />
+          )}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
