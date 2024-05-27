5c4e9262dfa32e39cb4a338e9b95ce7698192e4e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-27 10:09:50 -0500

    Add onClick event to NavbarButton component

--- src/components/app/navbar/navbar.button.tsx
+++ src/components/app/navbar/navbar.button.tsx
@@ -5,16 +5,18 @@ interface NavbarButtonProps {
   icon: LucideIcon;
   size?: number;
   classname?: string;
+  onClick?: () => void;
 }
 
 export function NavbarButton({
   icon: Icon,
   size = 20,
   classname,
+  onClick,
 }: NavbarButtonProps) {
   return (
     <>
-      <Button variant={"ghost"} className="w-8 h-8 p-0">
+      <Button variant={"ghost"} className="w-8 h-8 p-0" onClick={onClick}>
         {Icon && <Icon size={size} className={classname} />}
       </Button>
     </>
